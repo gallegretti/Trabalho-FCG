64f26057f750dce590f19ee06a41bac152ec5a04
gallegretti <gabriel13082@hotmail.com>
2017-12-17 11:16:56 -0200

    Change ClearColor to blue

--- src/main.cpp
+++ src/main.cpp
@@ -315,7 +315,7 @@ int main(int argc, char* argv[])
         // Conversaremos sobre sistemas de cores nas aulas de Modelos de Iluminação.
         //
         //           R     G     B     A
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClearColor(0.5f, 0.6f, 0.95f, 1.0f);
 
         // "Pintamos" todos os pixels do framebuffer com a cor definida acima,
         // e também resetamos todos os pixels do Z-buffer (depth buffer).
